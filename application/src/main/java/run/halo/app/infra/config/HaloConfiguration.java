7fb973b6a70c19b140b83b973e283afa7bae4704
Steven Chen <117523987+StevenChen16@users.noreply.github.com>
2024-11-26 11:49:29 -0500

    Update HaloConfiguration.java

--- application/src/main/java/run/halo/app/infra/config/HaloConfiguration.java
+++ application/src/main/java/run/halo/app/infra/config/HaloConfiguration.java
@@ -25,7 +25,6 @@ import run.halo.app.search.lucene.LuceneSearchEngine;
 @EnableCaching
 @Configuration(proxyBeanMethods = false)
 @EnableAsync
-@Data
 public class HaloConfiguration {
 
     private static final Logger log = LoggerFactory.getLogger(HaloConfiguration.class);
